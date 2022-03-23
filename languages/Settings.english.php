44d5f7f5221952fe529efe2c188740681d84a8b0
splinxs <102218554+splinxs@users.noreply.github.com>
2022-03-23 12:12:24 +0300

    Add files via upload

---
+++ languages/Settings.english.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.0; Settings
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Open modern design. <br /><br />Skyline by <a href="http://www.dzinerstudio.com">DzinerStudio</a>';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,37 @@
+<?php
+// Version: 2.0; Themes
+
+global $scripturl;
+
+$txt['quicknav_but1'] = 'Quick Link1 -> Button Name';
+$txt['quicknav_url1'] = 'Quick Link1 -> Button Url';
+$txt['quicknav_but2'] = 'Quick Link2 -> Button Name';
+$txt['quicknav_url2'] = 'Quick Link2 -> Button Url';
+$txt['quicknav_but3'] = 'Quick Link3 -> Button Name';
+$txt['quicknav_url3'] = 'Quick Link3 -> Button Url';
+$txt['quicknav_but4'] = 'Quick Link4 -> Button Name';
+$txt['quicknav_url4'] = 'Quick Link4 -> Button Url';
+$txt['quicknav_but5'] = 'Quick Link5 -> Button Name';
+$txt['quicknav_url5'] = 'Quick Link5 -> Button Url';
+$txt['quicknavs_desc'] = 'Above links will be displayed below menu';
+$txt['facebook_url'] = 'Facebook URL';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['googleplus_url'] = 'Google+ URL';
+$txt['dribble_url'] = 'Dribble URL';
+$txt['flickr_url'] = 'Flickr URL';
+$txt['youtube_url'] = 'Youtube URL';
+$txt['pinterest_url'] = 'Pinterest URL';
+$txt['rss_url'] = 'RSS URL';
+
+$txt['hi'] = 'Hi';
+$txt['show_unread'] = 'Okunmamýþ Ýletiler';
+$txt['show_replies'] = 'Deðiþiklik Olmuþ Konular';
+$txt['maintenance'] = 'Site Bakým Modunda!';
+$txt['approval_member'] = 'Member Approvals';
+$txt['open_reports'] = 'Open Reports';
+$txt['forum_search'] = 'Search...';
+$txt['board'] = '';
+$txt['latest'] = '';
+$txt['stats'] = '';
+
+?>
